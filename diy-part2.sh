6a1fd054e39aab31ef6d56a6d8fc5c7d694dfd84
powersee <ipowersee@outlook.com>
2021-07-04 20:07:52 +0800

    sed

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/R21./Compiled by powersee R21./g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' /feeds/luci/collections/luci/Makeile
